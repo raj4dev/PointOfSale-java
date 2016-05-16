7a76b9483cec0c86fd29dcc367397694ebb09244
rb <rb@email.com>
2016-05-16 15:09:18 -0700

    added test case to check empty barcodes.

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -33,6 +33,15 @@ public class SellOneItemTest {
         assertEquals("Product not found for 99999", display.getText());
     }
 
+    @Test
+    public void emptyBarcode() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("");
+        assertEquals("Scanning error: empty barcode", display.getText());
+    }
+
     /*###2*/
     public static class Display {
         private String text;
