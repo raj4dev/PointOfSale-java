4b46896aeb22486945fab3f8e68489ffaa6435cd
rb <rb@email.com>
2016-05-16 10:54:14 -0700

    added a failing test for the scenario 'product is found'

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -7,9 +7,27 @@ import static junit.framework.TestCase.assertEquals;
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale();
+
         sale.onBarcode("12345");/* ###1 */
         assertEquals("$7.95", display.getText());
     }
+
+    /*###2*/
+    public static class Display {
+        public String getText() {
+            return null;
+        }
+    }
+
+    /*Refer notes ###2*/
+    public static class Sale {
+        public void onBarcode(String barCode) {
+            /*No Return value !!! Event handlers do NOT return values*/
+        }
+    }
+
 }
 
 
@@ -23,4 +41,10 @@ NOTES
     incoming event on some object. Perhaps a "sale" object ? Now, the
     onBarcode name comes from event naming rules of Java, i.e. onSomethingHappening.
     The barcode of 12345 is random. Its a dummy barcode for an item which costs $7.95.
+
+
+###2 Created Display as inner class instead of separate for ease of learning. Public
+    keyword not needed at the moment. Static needed so that we don't have to
+    instantiate the above test class to use Display.
+
 */
