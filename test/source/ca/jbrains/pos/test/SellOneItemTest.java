719ddbb18c7d698f60fdadcea8fed3dd62d9bc31
rb <rb@email.com>
2016-05-16 22:05:58 -0700

    removed duplicate test setup.

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,6 +1,7 @@
 package ca.jbrains.pos.test;
 
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collections;
@@ -10,38 +11,33 @@ import java.util.Map;
 import static junit.framework.TestCase.assertEquals;
 
 public class SellOneItemTest {
-    @Test
-    public void productFound() throws Exception {
-        final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>() {{
+
+    private Display display;
+    private Sale sale;
+
+    @Before
+    public void setUp() throws Exception {
+        display = new Display();
+        sale = new Sale(display, new HashMap<String, String>() {{
             put("12345", "$7.95");
             put("23456", "$12.50");
         }});
+    }
 
+    @Test
+    public void productFound() throws Exception {
         sale.onBarcode("12345");/* ###1 */
         assertEquals("$7.95", display.getText());
     }
 
     @Test
     public void anotherProductFound() throws Exception {
-        final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>() {{
-            put("12345", "$7.95");
-            put("23456", "$12.50");
-        }});
-
         sale.onBarcode("23456");
         assertEquals("$12.50", display.getText());
     }
 
     @Test
     public void productNotFound() throws Exception {
-        final Display display = new Display();
-        final Sale sale = new Sale(display, new HashMap<String, String>() {{
-            put("12345", "$7.95");
-            put("23456", "$12.50");
-        }});
-
         sale.onBarcode("99999");
         assertEquals("Product not found for 99999", display.getText());
     }
