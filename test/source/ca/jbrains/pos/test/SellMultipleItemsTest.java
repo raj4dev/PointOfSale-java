d4cfe20caddd3f23c1c42e32cd7787a80586ffb6
rb <rb@email.com>
2016-05-20 00:01:43 -0700

    apparently, we could already 'sell' one item if we have no price for it.(That sounds wrong.)

--- test/source/ca/jbrains/pos/test/SellMultipleItemsTest.java
+++ test/source/ca/jbrains/pos/test/SellMultipleItemsTest.java
@@ -20,9 +20,20 @@ public class SellMultipleItemsTest {
         Catalog catalog = new Catalog(Collections.singletonMap("12345", "$6.50"));
         Display display = new Display();
         Sale sale = new Sale(display, catalog);
-        sale.onBarcode("12345");
 
+        sale.onBarcode("12345");
         sale.onTotal();
         assertEquals("Total: $6.50", display.getText());
+    }
+
+    @Test
+    public void oneItemNotFound() throws Exception {
+        Catalog catalog = new Catalog(Collections.singletonMap("12345", "$6.50"));
+        Display display = new Display();
+        Sale sale = new Sale(display, catalog);
+
+        sale.onBarcode("99999");
+        sale.onTotal();
+        assertEquals("No sale in progress. Try scanning a product.", display.getText());
     }
 }
