9813e201b48dbf8535583cf991808d1d1828b91a
rb <rb@email.com>
2016-05-22 16:01:17 -0700

    added a Catalog constructor with 2 hashmap and deprecated the older one.

--- test/source/ca/jbrains/pos/test/Catalog.java
+++ test/source/ca/jbrains/pos/test/Catalog.java
@@ -5,11 +5,15 @@ import java.util.Map;
 
 public class Catalog {
     private final Map<String, String> pricesByBarcode;
+    private final Map<String, String> pricesInCentsByBarcode;
 
+    /** @@deprecated */
     Catalog(Map<String, String> pricesByBarcode) {
         this.pricesByBarcode = pricesByBarcode;
     }
 
+    public Catalog(HashMap<String, String> pricesAsTextByBarcode, HashMap<String, Integer> pricesInCentsByBarcode) {
+    }
 
 
     public String findPrice(String barCode) {
