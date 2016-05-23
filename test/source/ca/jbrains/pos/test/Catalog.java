a4f13ecbb6ba302c3539c5a1e4a79cb9e363ea93
rb <rb@email.com>
2016-05-23 12:36:51 -0700

    Finally replaced String prices with Integer prices so that I can do arithmetic on them.

--- test/source/ca/jbrains/pos/test/Catalog.java
+++ test/source/ca/jbrains/pos/test/Catalog.java
@@ -7,13 +7,6 @@ public class Catalog {
     private Map<String, String> pricesAsTextByBarcode;
     private Map<String, Integer> pricesInCentsByBarcode;
 
-    /**
-     * @@deprecated
-     */
-    Catalog(Map<String, String> pricesByBarcode) {
-        this.pricesAsTextByBarcode = pricesByBarcode;
-    }
-
     Catalog(HashMap<String, String> pricesAsTextByBarcode, HashMap<String, Integer> pricesInCentsByBarcode) {
         this.pricesAsTextByBarcode = pricesAsTextByBarcode;
         this.pricesInCentsByBarcode = pricesInCentsByBarcode;
