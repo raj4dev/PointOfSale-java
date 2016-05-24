a49114e1d1720c8819a2457c50ac1cf3b3dd1996
rb <rb@email.com>
2016-05-24 12:34:32 -0700

    preparing to use parameterized tests for the features defined by FormatMonetaryAmountTest.

--- test/source/ca/jbrains/pos/test/FormatMonetaryAmountTest.java
+++ test/source/ca/jbrains/pos/test/FormatMonetaryAmountTest.java
@@ -1,11 +1,26 @@
 package ca.jbrains.pos.test;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static junit.framework.TestCase.assertEquals;
 
-
+@RunWith(Parameterized.class)
 public class FormatMonetaryAmountTest {
+
+    public FormatMonetaryAmountTest(int priceInCents, String expectedFormattedPrice) {
+
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{{789, "$7.89"}});
+    }
+
     @Test
     public void simplest() throws Exception {
         assertEquals("$7.89", format(789));
