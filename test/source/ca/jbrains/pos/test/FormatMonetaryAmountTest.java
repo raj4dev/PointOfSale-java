ee79265b98c4700e9388c29a88b231812f61fa46
rb <rb@email.com>
2016-05-24 12:07:17 -0700

    added a new feature test list FormatMoneyAmountTest, with one scenario test 'simple'

---
+++ test/source/ca/jbrains/pos/test/FormatMonetaryAmountTest.java
@@ -0,0 +1,13 @@
+package ca.jbrains.pos.test;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class FormatMonetaryAmountTest {
+    @Test
+    public void simplest() throws Exception {
+        assertEquals("$7.89", format(789));
+    }
+}
