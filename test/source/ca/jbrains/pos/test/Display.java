c870acf81d6403a61cabdbcbd6eb5637d4f35ffa
rb <rb@email.com>
2016-05-25 10:38:41 -0700

    FIXED the comments in code !!!

--- test/source/ca/jbrains/pos/test/Display.java
+++ test/source/ca/jbrains/pos/test/Display.java
@@ -3,7 +3,6 @@ package ca.jbrains.pos.test;
 public class Display {
     public String text;/*Make this private later*/
 
-    //SMELL move this behavior somewhere else.
     public static String format(int priceInCents) {
         return String.format("$%,.2f", priceInCents / 100.0d);/*###1*/
     }
@@ -32,3 +31,27 @@ public class Display {
         this.text = format(priceInCents);
     }
 }
+
+/*
+* --------------------------------------------------------------
+* NOTES
+* --------------------------------------------------------------
+*
+* ###1
+*
+* We divide by 100.0 to get a decimal result & not a rounded off integer;
+* Not sure if d is needed. Code works without it;
+*
+* $ prints the dollar sign as is;
+* The % marks the beginning of a 'format specifier' or the format in which a
+* a particular argument of format() is to be printed. The format specifier
+* ends with a 'converter';
+*
+* ,.2f is a 'format specifier'  which means print only two places after a decimal
+* of the desired argument. If the arg has only 1 decimal, then a zero will be added.
+* The f is a 'converter' which marks the end of the format specifier. The comma
+* tells format() to group the whole number part of the argument per locale-specific
+* grouping. That is, Americans will get "4,189.76", Germans will get '4.189.76'
+* & Swiss Germans will get 4'189'76 etc.
+*
+* */
--- test/source/ca/jbrains/pos/test/FormatMonetaryAmountTest.java
+++ test/source/ca/jbrains/pos/test/FormatMonetaryAmountTest.java
@@ -46,22 +46,5 @@ public class FormatMonetaryAmountTest {
 * --------------------------------------------------------------
 * NOTES
 * --------------------------------------------------------------
-*
-* ###1
-*
-* We divide by 100.0 to get a decimal result & not a rounded off integer;
-* Not sure if d is needed. Code works without it;
-*
-* $ prints the dollar sign as is;
-* The % marks the beginning of a 'format specifier' or the format in which a
-* a particular argument of format() is to be printed. The format specifier
-* ends with a 'converter';
-*
-* ,.2f is a 'format specifier'  which means print only two places after a decimal
-* of the desired argument. If the arg has only 1 decimal, then a zero will be added.
-* The f is a 'converter' which marks the end of the format specifier. The comma
-* tells format() to group the whole number part of the argument per locale-specific
-* grouping. That is, Americans will get "4,189.76", Germans will get '4.189.76'
-* & Swiss Germans will get 4'189'76 etc.
-*
+* NONE !!!
 * */
