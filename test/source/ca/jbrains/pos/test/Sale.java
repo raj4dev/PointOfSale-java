3145a5971c421e079f7ad486673a3b1ad26e9bc0
rb <rb@email.com>
2016-05-25 16:23:23 -0700

    Removed obsolete comments !

--- test/source/ca/jbrains/pos/test/Sale.java
+++ test/source/ca/jbrains/pos/test/Sale.java
@@ -3,7 +3,6 @@ package ca.jbrains.pos.test;
 import java.util.ArrayList;
 import java.util.Collection;
 
-/*Refer notes ###2*/
 public class Sale {
     private final Catalog catalog;
     private Display display;
@@ -16,7 +15,7 @@ public class Sale {
     }
 
     public void onBarcode(String barCode) {
-        /*###3*/
+        /*###1*/
         if ("".equals(barCode)) {
             display.displayEmptyBarcodeMessage();
             return; /*A guard clause*/
@@ -64,11 +63,7 @@ public class Sale {
 NOTES
 ---------------------------------------------
 
-###2 Created Display as inner class instead of separate for ease of learning. Public
-    keyword not needed at the moment. Static needed so that we don't have to
-    instantiate the above test class to use Display.
-
-###3 Code smell: "Refused Bequest": move this up the call stack? Perhaps onBarcode
+###1 Code smell: "Refused Bequest": move this up the call stack? Perhaps onBarcode
         should not check validity of a barcode & something else should do it. Now,
         the 1st if validates data & the next one processes it. Maybe we should do
         the data validation in another function. Lets see.
