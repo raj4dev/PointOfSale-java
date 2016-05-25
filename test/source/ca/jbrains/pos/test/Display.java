9c9bb68595414414be754446b9b537d4af505322
rb <rb@email.com>
2016-05-25 10:31:53 -0700

    renamed a variable.

--- test/source/ca/jbrains/pos/test/Display.java
+++ test/source/ca/jbrains/pos/test/Display.java
@@ -12,8 +12,8 @@ public class Display {
         return text;
     }
 
-    public void displayText(String priceAsText) {
-        this.text = priceAsText;
+    public void displayText(String text) {
+        this.text = text;
     }
 
     public void displayProductNotFoundMessage(String barCode) {
