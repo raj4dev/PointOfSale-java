28259a3b70b2386f25d80b2708e1646649389faf
rb <rb@email.com>
2016-05-25 17:03:16 -0700

    added a new failing test case

--- test/source/ca/jbrains/pos/test/FormatMonetaryAmountTest.java
+++ test/source/ca/jbrains/pos/test/FormatMonetaryAmountTest.java
@@ -8,7 +8,7 @@ import org.junit.runners.Parameterized.Parameters;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class FormatMonetaryAmountTest {
--- test/source/ca/jbrains/pos/test/ScanOneItemTest.java
+++ test/source/ca/jbrains/pos/test/ScanOneItemTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class ScanOneItemTest {
 
