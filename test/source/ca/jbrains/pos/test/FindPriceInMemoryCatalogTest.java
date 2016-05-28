f25d963a6a9a28cb437e05de83a8d8810dad04cf
rb <rb@email.com>
2016-05-27 18:59:30 -0700

    made test data more self describing.

--- test/source/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ test/source/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -26,7 +26,7 @@ public class FindPriceInMemoryCatalogTest {
     }
 
     private Catalog catalogWithout(String barcodeToAvoid) {
-        return new InMemoryCatalog(Collections.emptyMap());
+        return new InMemoryCatalog(Collections.singletonMap("anything but " + barcodeToAvoid, Price.cents(0)));
     }
 
     public static class InMemoryCatalog implements Catalog {
