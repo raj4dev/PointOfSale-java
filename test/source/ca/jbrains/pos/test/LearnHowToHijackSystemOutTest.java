da6e450340f4f592fb4294a0a534dc8a95bf7c69
rb <rb@email.com>
2016-06-02 16:26:57 -0700

    added code to make the test compile. Test run fails because we have not written anything to our ByteArrayOutputStream.

--- test/source/ca/jbrains/pos/test/LearnHowToHijackSystemOutTest.java
+++ test/source/ca/jbrains/pos/test/LearnHowToHijackSystemOutTest.java
@@ -2,16 +2,18 @@ package ca.jbrains.pos.test;
 
 import org.junit.Test;
 
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.assertEquals;
 
 public class LearnHowToHijackSystemOutTest {
     @Test
     public void singleLineOfText() throws Exception {
         //Action of this test should be writing a single line to System.out.
+        ByteArrayOutputStream canvas = new ByteArrayOutputStream();
         System.out.println("Hello, world.");
 
         //Assertion of this test should be to check the ByteArrayOutputStream & get 'hello world' back.
-        assertEquals("Hello, world.", actualTest);
+        assertEquals("Hello, world.", canvas.toString("UTF-8"));
     }
 }
