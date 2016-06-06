ed781190b28712f558cf2cf60563055e5e726c5e
rb <rb@email.com>
2016-06-06 16:33:57 -0700

    enabled the test to run independently of line breaks in our input test.

--- test/source/ca/jbrains/pos/test/LearnHowToHijackSystemOutTest.java
+++ test/source/ca/jbrains/pos/test/LearnHowToHijackSystemOutTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,6 +28,8 @@ public class LearnHowToHijackSystemOutTest {
 
     @Test
     public void singleLineOfText() throws Exception {
+        System.out.println("One line of text.");
+
         //Action of this test should be writing a single line to System.out.
         ByteArrayOutputStream canvas = new ByteArrayOutputStream();
         System.setOut(new PrintStream(canvas));
@@ -32,6 +37,30 @@ public class LearnHowToHijackSystemOutTest {
         System.out.println("Hello, world.");
 
         //Assertion of this test should be to check the ByteArrayOutputStream & get 'hello world' back.
-        assertEquals("Hello, world.\r\n", canvas.toString("UTF-8"));
+        assertEquals(Collections.singletonList("Hello, world."), lines(canvas.toString("UTF-8")));
+    }
+
+    @Test
+    public void severalLinesOfText() throws Exception {
+        /*
+        * This test checks if the code changes we made to avoid the problem of newline character
+        * work for multiple line input or not.
+        * */
+        System.out.println("multiple lines of text.");
+
+        ByteArrayOutputStream canvas = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(canvas));
+
+        for(int i =1; i<= 5; i++){
+            System.out.println("Line " + i);
+        }
+
+        assertEquals(Arrays.asList("Line 1","Line 2","Line 3","Line 4","Line 5"), lines(canvas.toString("UTF-8")));
+    }
+
+    //REFACTOR move this into a reusable library OR
+    //find a library which implements this more reliably.
+    private List<String> lines(String text) {
+       return Arrays.asList(text.split(System.lineSeparator()));
     }
 }
