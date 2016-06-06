85b3c17a2a6d25b3452c9e1fd06404dc75f124a7
rb <rb@email.com>
2016-06-06 15:56:06 -0700

    passed the test by printing the desired output to a stream.

--- test/source/ca/jbrains/pos/test/LearnHowToHijackSystemOutTest.java
+++ test/source/ca/jbrains/pos/test/LearnHowToHijackSystemOutTest.java
@@ -1,19 +1,37 @@
 package ca.jbrains.pos.test;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class LearnHowToHijackSystemOutTest {
+
+    private PrintStream productionSystemOut;
+
+    @Before
+    public void rememberSystemOut() {
+        productionSystemOut = System.out;
+    }
+
+    @After
+    public void restoreSystemOut() {
+        System.setOut(productionSystemOut);
+    }
+
     @Test
     public void singleLineOfText() throws Exception {
         //Action of this test should be writing a single line to System.out.
         ByteArrayOutputStream canvas = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(canvas));
+
         System.out.println("Hello, world.");
 
         //Assertion of this test should be to check the ByteArrayOutputStream & get 'hello world' back.
-        assertEquals("Hello, world.", canvas.toString("UTF-8"));
+        assertEquals("Hello, world.\r\n", canvas.toString("UTF-8"));
     }
 }
