985eb34d8d1a9fd1f51c77533d43260ccfdfae90
rb <rb@email.com>
2016-06-07 16:21:04 -0700

    inlined a private method just so that the user of the method looks similar to the other methods in its class.

--- test/source/ca/jbrains/pos/test/ConsoleDisplay.java
+++ test/source/ca/jbrains/pos/test/ConsoleDisplay.java
@@ -1,9 +1,6 @@
 package ca.jbrains.pos.test;
 
 public class ConsoleDisplay {
-    private static String formatPrice(Price price) {
-        return String.format("$%,.2f", price.dollarValue());
-    }
 
     public void displayProductNotFoundMessage(String barcodeNotFound) {
         System.out.println("Product not found for " + barcodeNotFound);
@@ -14,6 +11,6 @@ public class ConsoleDisplay {
     }
 
     public void displayPrice(Price price) {
-        System.out.println(formatPrice(price));
+        System.out.println(String.format("$%,.2f", price.dollarValue()));
     }
 }
