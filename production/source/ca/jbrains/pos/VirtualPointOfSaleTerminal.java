7aeb5d30ca0d3685d3112544cc198172f437848f
rb <rb@email.com>
2016-06-08 16:14:35 -0700

    Added old files which I forgot to add. Also added a virtual pos class which will show that our design works.

---
+++ production/source/ca/jbrains/pos/VirtualPointOfSaleTerminal.java
@@ -0,0 +1,6 @@
+package ca.jbrains.pos;
+
+public class VirtualPointOfSaleTerminal {
+    public static void main(String[] args) {
+    }
+}
---
+++ test/source/ca/jbrains/pos/test/EnglishLanguageConsoleDisplay.java
@@ -0,0 +1,28 @@
+package ca.jbrains.pos.test;
+
+public class EnglishLanguageConsoleDisplay implements Display {
+
+    public static final String PRODUCT_NOT_FOUND_MESSAGE_FORMAT = "Product not found for %s";
+    public static final String SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT = "Scanning error: empty barcode.";
+    public static final String PRICE_IN_DOLLARS_MESSAGE_FORMAT = "$%,.2f";
+
+    private void render(String text) {
+        System.out.println(text);
+    }
+
+    private String mergeTemplate(String messageTemplate, Object... placeholderValues) {
+        return String.format(messageTemplate, placeholderValues);
+    }
+
+    public void displayProductNotFoundMessage(String barcodeNotFound) {
+        render(mergeTemplate(PRODUCT_NOT_FOUND_MESSAGE_FORMAT, barcodeNotFound));
+    }
+
+    public void displayEmptyBarcodeMessage() {
+        render(mergeTemplate(SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT));
+    }
+
+    public void displayPrice(Price price) {
+        render(mergeTemplate(PRICE_IN_DOLLARS_MESSAGE_FORMAT, price.dollarValue()));
+    }
+}
