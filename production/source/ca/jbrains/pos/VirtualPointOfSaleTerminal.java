9574eee10f6a7785bdc075f9e215505340aac83e
rb <rb@email.com>
2016-06-08 16:33:33 -0700

    made a simple end to end 'test' of our POS system which can scan only one item for now.

--- production/source/ca/jbrains/pos/VirtualPointOfSaleTerminal.java
+++ production/source/ca/jbrains/pos/VirtualPointOfSaleTerminal.java
@@ -5,6 +5,13 @@ import java.util.HashMap;
 public class VirtualPointOfSaleTerminal {
     public static void main(String[] args) {
         SaleController saleController = new SaleController(new InMemoryCatalog(new HashMap<String, Price>() {{
+            put("12345", Price.cents(795));
+            put("23456", Price.cents(1250));
         }}), new EnglishLanguageConsoleDisplay());
+
+        saleController.onBarcode("12345");
+        saleController.onBarcode("23456");
+        saleController.onBarcode("99999");
+        saleController.onBarcode("");
     }
 }
