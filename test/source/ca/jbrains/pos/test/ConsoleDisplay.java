cd6b097d02ac4ac8914cfd7e31e31a409b48a29f
rb <rb@email.com>
2016-06-08 10:57:11 -0700

    making all method signatures the same. one method remains.

--- test/source/ca/jbrains/pos/test/ConsoleDisplay.java
+++ test/source/ca/jbrains/pos/test/ConsoleDisplay.java
@@ -6,12 +6,12 @@ public class ConsoleDisplay {
     public static final String SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT = "Scanning error: empty barcode.";
     public static final String PRICE_IN_DOLLARS_MESSAGE_FORMAT = "$%,.2f";
 
-    public void displayProductNotFoundMessage(String barcodeNotFound) {
+    public void displayProductNotFoundMessage(Object... placeholders) {
         System.out.println(
-                String.format(PRODUCT_NOT_FOUND_MESSAGE_FORMAT, barcodeNotFound));
+                String.format(PRODUCT_NOT_FOUND_MESSAGE_FORMAT, placeholders));
     }
 
-    public void displayEmptyBarcodeMessage() {
+    public void displayEmptyBarcodeMessage(Object... placeholders) {
         System.out.println(
                 String.format(SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT));
     }
