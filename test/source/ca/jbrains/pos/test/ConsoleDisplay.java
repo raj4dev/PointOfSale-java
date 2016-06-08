74ae1ac06a9f69dcf842419186ebf6d52a60ebb9
rb <rb@email.com>
2016-06-08 10:54:13 -0700

    replacing method outputs by constants. goal is to replace three methods of ConsoleDisplay with one method.

--- test/source/ca/jbrains/pos/test/ConsoleDisplay.java
+++ test/source/ca/jbrains/pos/test/ConsoleDisplay.java
@@ -2,18 +2,22 @@ package ca.jbrains.pos.test;
 
 public class ConsoleDisplay {
 
+    public static final String PRODUCT_NOT_FOUND_MESSAGE_FORMAT = "Product not found for %s";
+    public static final String SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT = "Scanning error: empty barcode.";
+    public static final String PRICE_IN_DOLLARS_MESSAGE_FORMAT = "$%,.2f";
+
     public void displayProductNotFoundMessage(String barcodeNotFound) {
         System.out.println(
-                String.format("Product not found for %s", barcodeNotFound));
+                String.format(PRODUCT_NOT_FOUND_MESSAGE_FORMAT, barcodeNotFound));
     }
 
     public void displayEmptyBarcodeMessage() {
         System.out.println(
-                String.format("Scanning error: empty barcode."));
+                String.format(SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT));
     }
 
     public void displayPrice(Price price) {
         System.out.println(
-                String.format("$%,.2f", price.dollarValue()));
+                String.format(PRICE_IN_DOLLARS_MESSAGE_FORMAT, price.dollarValue()));
     }
 }
